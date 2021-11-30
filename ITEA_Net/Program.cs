3018785e4736ca674a6038c226a061cbd0fc6358
Euvgen Balakov <t0503397771@gmail.com>
2021-11-30 09:28:59 +0200

    Add project files.

---
+++ ITEA_Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITEA_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
